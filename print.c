47938673ef311817974a39ade434e8084ef7f892
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-05-25 08:36:28 -0700

    Functionnthat prints all the elements in the stack

---
+++ print.c
@@ -0,0 +1,19 @@
+#include"monty.h"
+/**
+ * pall - print function
+ * Description: Function that prints elements in the stack
+ * @topptr: pointer to the top node in the stack
+*/
+
+void pall(stack_t **topptr)
+{
+	stack_t *temp;
+
+	temp = *topptr;
+
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->prev;
+	}
+}
