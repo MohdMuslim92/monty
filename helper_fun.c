dd1a5d8f9ee4575b20bc4d7c9bae9c5708d011ad
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-05-25 19:37:57 -0700

    Add function that handles the add instruction by calling the add function which add the two top elements

--- helper_fun.c
+++ helper_fun.c
@@ -17,6 +17,25 @@ void handleSwap(stack_t **ptr, int line_num, int *err_flag)
 		return;
 	}
 }
+
+/**
+ * handleAdd - helper function
+ * Description: a function that calls another function to
+ *				add the value of the top two elements in the stack
+ * @ptr: pointer to the top of the stack
+ * @line_num: the line number which used when printing an error if happened
+ * @err_flag: an error flag that it's value 0 and will be
+ *				updatecld if an error happened
+*/
+void handleAdd(stack_t **ptr, int line_num, int *err_flag)
+{
+	add(ptr, err_flag);
+	if (*err_flag == 1)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_num);
+		return;
+	}
+}
 /**
  * handleUnknownInstruction - helper function
  * Description: a function that print an error to the standard
