f53f96e91beac4e106e2bdcd12f21afd164763a7
MohdMuslim92 <mohammed.muslim2022@gmail.com>
2023-05-28 20:55:04 +0000

    Implement additional stack-related functions
    - Added handleDiv function to perform division on the stack
      - The function divides the second top element by the top element and removes the top element from the stack
    
    - Added handleMod function to compute the remainder of division on the stack
      - The function computes the remainder of dividing the second top element by the top element and removes the top element from the stack
    
    - Moved handlePall function from parse.c to helperfun1.c
      - The function now resides in the helperfun1.c file and calls a printing function to display all elements in the stack
    
    - Moved handlePint function from parse.c to helperfun1.c
      - The function now resides in the helperfun1.c file and calls another function to display the value of the top element in the stack
    
    - Moved handlePop function from parse.c to helperfun1.c
      - The function now resides in the helperfun1.c file and calls another function to remove the top element from the stack
    
    These additional functions enhance the functionality of the Monty interpreter by providing operations for division, modulo, printing all elements, printing the top element, and removing the top element in the stack. The relevant functions have been moved from the parse.c file to the helperfun1.c file, allowing for better organization and modularity within the codebase.

---
+++ helper_fun1.c
@@ -0,0 +1,97 @@
+#include"monty.h"
+/**
+ * handleDiv - helper function
+ * Description: a function that calls another function to
+ *				devide the value of the second top two element of the stack by the top
+ *				element of the stack
+ * @ptr: pointer to the top of the stack
+ * @line_num: the line number which used when printing an error if happened
+ * @err_flag: an error flag that it's value 0 and will be
+ *				updatecld if an error happened
+*/
+void handleDiv(stack_t **ptr, int line_num, int *err_flag)
+{
+	division(ptr, err_flag);
+	if (*err_flag == 1)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_num);
+		return;
+	}
+}
+
+
+/**
+ * handleMod - helper function
+ * Description: a function that calls another function to
+ *				computes the rest of the division of the second top element of the stack
+ *				by the top element of the stack
+ * @ptr: pointer to the top of the stack
+ * @line_num: the line number which used when printing an error if happened
+ * @err_flag: an error flag that it's value 0 and will be
+ *				updatecld if an error happened
+*/
+void handleMod(stack_t **ptr, int line_num, int *err_flag)
+{
+	mod(ptr, err_flag);
+	if (*err_flag == 1)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_num);
+		return;
+	}
+	if (*err_flag == 2)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_num);
+		*err_flag = 1;
+		return;
+	}
+}
+
+/**
+ * handlePall - helper function
+ * Description: a function that calls printing function to
+ *				print stack elements
+ * @ptr: pointer to the top of the stack
+ *				updatecld if an error happened
+*/
+void handlePall(stack_t **ptr)
+{
+	pall(ptr);
+}
+
+/**
+ * handlePint - helper function
+ * Description: a function that calls another function to
+ *				print the top element in the stack
+ * @ptr: pointer to the top of the stack
+ * @line_num: the line number which used when printing an error if happened
+ * @err_flag: an error flag that it's value 0 and will be
+ *				updatecld if an error happened
+*/
+void handlePint(stack_t **ptr, int line_num, int *err_flag)
+{
+	pint(ptr, err_flag);
+	if (*err_flag == 1)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_num);
+		return;
+	}
+}
+
+/**
+ * handlePop - helper function
+ * Description: a function that calls another function to
+ *				remove the top element in the stack
+ * @ptr: pointer to the top of the stack
+ * @line_num: the line number which used when printing an error if happened
+ * @err_flag: an error flag that it's value 0 and will be
+ *				updatecld if an error happened
+*/
+void handlePop(stack_t **ptr, int line_num, int *err_flag)
+{
+	pop(ptr, err_flag);
+	if (*err_flag == 1)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_num);
+		return;
+	}
+}
