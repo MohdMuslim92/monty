15ef00f693e46998dc05d16a7880a8c23aa76b46
MohdMuslim92 <mohammed.muslim2022@gmail.com>
2023-05-28 23:34:25 +0000

    Create pchar.c file
    - Created a new file called pchar.c to hold the function body for the pchar function.
    - The pchar function is responsible for printing the character at the top of the stack, followed by a new line.
    - This separation of function implementations into dedicated files improves code readability and maintainability.
    
    The addition of the pchar.c file provides a dedicated place for the implementation of the pchar function within the Monty interpreter's codebase.

---
+++ pchar.c
@@ -0,0 +1,32 @@
+#include"monty.h"
+/**
+ * pchar - letter print function
+ * Description: Function that prints the char at the top of the stack, followed
+ * by a new line
+ * @topptr: pointer to the top node in the stack
+ * @err_flag: a flag that it's value is 0 and will be updated
+ *				if the stack is empty
+*/
+
+void pchar(stack_t **topptr, int *err_flag)
+{
+	stack_t *temp;
+	int num;
+	char letter;
+
+	if (*topptr == NULL)
+	{
+		*err_flag = 1;
+		return;
+	}
+	num = (*topptr)->n;
+	if ((num >= 65 && num <= 90) || (num >= 97 && num <= 122))
+	{
+		letter = (char)num;
+		printf("%c\n", letter);
+	} else
+	{
+		*err_flag = 2;
+		return;
+	}
+}
