378c93f8d97c91eef1bfe05aee96687db339e7bf
MohdMuslim92 <mohammed.muslim2022@gmail.com>
2023-05-28 23:28:39 +0000

    Move handlePush function body to helperfun2.c
    - Moved the body of the handlePush function from parse.c to helperfun2.c to separate function implementations from function calls.
    - The handlePush function is responsible for pushing values onto the stack based on the specified command.
    
    By moving the handlePush function body to helperfun2.c, the codebase of the Monty interpreter becomes more modular and organized. The parse.c file now focuses solely on function calls, while the implementation details of handlePush are contained within helperfun2.c. This separation improves code readability and maintainability, making it easier to manage and extend the functionality of the Monty interpreter.

--- parse.c
+++ parse.c
@@ -80,39 +80,3 @@ void processLine(char *line, stack_t **ptr, int line_num, int *err_flag)
 		token = strtok(NULL, delimiter);
 	}
 }
-
-/**
- * handlePush - helper function
- * Description: a function that tokenize the line and then call
- *				the function push and pass the number to it
- *				to push an element to the stack
- *@token: pointer to token to be tokenize
- * @ptr: pointer to the top of the stack
- * @line_num: the line number which used when printing an error if happened
- * @err_flag: an error flag that it's value 0 and will be
- *				updatecld if an error happened
-*/
-void handlePush(char *token, stack_t **ptr, int line_num, int *err_flag)
-{
-	char *strptr;
-	long int number;
-
-	if (token != NULL)
-	{
-		number = strtol(token, &strptr, 10);
-		if (strptr == token)
-		{
-			fprintf(stderr, "L%d: usage: push integer\n", line_num);
-			*err_flag = 1;
-			return;
-		}
-		push(ptr, number);
-	}
-	else
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_num);
-		*err_flag = 1;
-		return;
-	}
-}
-
